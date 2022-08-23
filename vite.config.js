56b4099bdfe2dc4a41cd6682a847c3be31791506
pengshiyu <1940607002@qq.com>
2022-08-23 21:40:20 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  console.log('mode:', mode)
 
-  server: {
-    open: true,
-  },
+  return {
+    plugins: [vue()],
 
-  manualChunks: {
-    vue: ['vue']
+    base: mode == 'production' ? '/moon-cake/' : '/',
+
+    server: {
+      open: true,
+    },
   }
 })
